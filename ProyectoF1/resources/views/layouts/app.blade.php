c2fd2fd4dedb3391e7ee200b505958fdbd111039
Jose Ponferrada <joseponfe10@gmail.com>
2025-03-04 12:04:21 +0100

    fix(Proyecto): added logo to show it on every tab

--- ProyectoF1/resources/views/layouts/app.blade.php
+++ ProyectoF1/resources/views/layouts/app.blade.php
@@ -8,6 +8,10 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/logo-f1-laravel.svg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/logo-f1-laravel.svg') }}" type="image/x-icon">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
